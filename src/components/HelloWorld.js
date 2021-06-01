0a22bad9b87e915a6fb3c6de9998e2e95fb3838e
Enyabuto <elishahyperx@yahoo.com>
2021-06-01 17:55:18 -0500

    Project set up

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+export const HelloWorld = () => {
+    //return React.createElement('div', null,
+    //    React.createElement(
+    //        'h1',
+    //        null,
+    //        'HelloWorld!!!',
+    //    ),
+    //    React.createElement(
+    //        'span',
+    //        null,
+    //        'Test'
+    //    ),
+    //);
+  return <>
+      <h1> Hello World!! </h1>
+      <span>Test</span>
+    </>
+}; //Arrow Function
+//Arrow functions better reflects what I am trying to accomplish
+
+export default HelloWorld;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { CarTool } from "./components/CarTool";
+const carList = [
+  {
+    id: 1,
+    make: "Ford",
+    model: "Fusion Hybrid",
+    year: 2018,
+    color: "silver",
+    price: 30000,
+  },
+  {
+    id: 2,
+    make: "Nissan",
+    model: "Rogue",
+    year: 2021,
+    color: "red",
+    price: 35000,
+  },
+];
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <CarTool cars={carList}/>,
+  document.querySelector("#root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
