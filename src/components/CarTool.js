59b87adbc2eeb7847eb103020b5b483f7c889154
Enyabuto <elishahyperx@yahoo.com>
2021-06-01 19:06:56 -0500

    React Hooks Introduced

--- src/components/CarTool.js
+++ src/components/CarTool.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const CarTool = (props) => {
+export const CarTool = ({cars:intialCars}) => {
+  const [
+    carForm, // state data
+    setCarForm, // function to update the state data and re-render.
+  ] = useState(
+    {
+      make: "",
+      model: "",
+      year: 1900,
+      color: "",
+      price: 0,
+    } /* intial state data, and is only used on the intial render */
+    );
   
+  const [cars,setCars] = useState(intialCars.concat())
+
+  const change = ({target: {name, type, value}}) => {
+    setCarForm({
+      ...carForm,
+      [name]:
+        type === "number" ? Number(value) : value,
+    });
+  };
+
+  const addCar = () => {
+    setCars(cars.concat({
+      ...carForm,
+      id: Math.max(...cars.map(c => c.id)) + 1,
+    }));
+
+    setCarForm({
+      make: '',
+      model: '',
+      year: 1900,
+      color: '',
+      price: 0,
+    });
+  };
   return (
     <>
       <header>
@@ -19,7 +55,7 @@ export const CarTool = (props) => {
           </tr>
         </thead>
         <tbody>
-          {props.cars.map((car) => (
+          {cars.map((car) => (
             <tr key={car.id}>
               <td>{car.id}</td>
               <td>{car.make}</td>
@@ -31,6 +67,59 @@ export const CarTool = (props) => {
           ))}
         </tbody>
       </table>
+      <form>
+        <div>
+          <label htmlFor="make-input">Make:</label>
+          <input
+            type="text"
+            id="make-input"
+            name="make"
+            value={carForm.make}
+            onChange={change}
+          />
+        </div>
+        <div>
+          <label htmlFor="model-input">Model:</label>
+          <input
+            type="text"
+            id="model-input"
+            name="model"
+            value={carForm.model}
+            onChange={change}
+          />
+        </div>
+        <div>
+          <label htmlFor="year-input">Year:</label>
+          <input
+            type="number"
+            id="year-input"
+            name="year"
+            value={carForm.year}
+            onChange={change}
+          />
+        </div>
+        <div>
+          <label htmlFor="color-input">Color:</label>
+          <input
+            type="text"
+            id="color-input"
+            name="color"
+            value={carForm.color}
+            onChange={change}
+          />
+        </div>
+        <div>
+          <label htmlFor="price-input">Price:</label>
+          <input
+            type="number"
+            id="price-input"
+            name="price"
+            value={carForm.price}
+            onChange={change}
+          />
+        </div>
+        <button type="button" onClick={addCar}>Add Car</button>
+      </form>
     </>
   );
 };
